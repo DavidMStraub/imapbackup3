1e3dc103220167ac27afe02436d40d1a7b7709a4
David Straub <david.straub@tum.de>
2019-10-10 17:58:22 +0200

    Turn into installable package

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md") as f:
+    LONG_DESCRIPTION = f.read()
+
+
+setup(
+    name="imapbackup3",
+    version="0.1",
+    author="David M. Straub <straub@pm.me>",
+    author_email="straub@pm.me",
+    url="https://github.com/DavidMStraub/imapbackup3",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    packages=find_packages(),
+    entry_points={"console_scripts": ["imapbackup3=imapbackup3.cli:main"]},
+)
